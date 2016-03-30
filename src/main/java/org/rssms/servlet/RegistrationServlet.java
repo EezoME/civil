371716a5a642961bf236e65302eb244268e8c611
h484558 <h484558@hotmail.com>
2016-03-30 23:30:52 +0300

    Fix redirect in RegistrationServlet

--- src/main/java/org/rssms/servlet/RegistrationServlet.java
+++ src/main/java/org/rssms/servlet/RegistrationServlet.java
@@ -18,6 +18,6 @@ public class RegistrationServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("signup.jsp").forward(request, response);
+        request.getRequestDispatcher("/signup.jsp").forward(request, response);
     }
 }
